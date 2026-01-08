fd75c0adc56a658c09e462ffcccc9f2d5e02cf20
vishaku03 <vishalk.shrivastav1@gmail.com>
2026-01-08 20:01:59 +0530

    LC

--- Coding2026/day4.cpp
+++ Coding2026/day4.cpp
@@ -30,3 +30,46 @@ public:
           return atMost(arr, k) - atMost(arr, k - 1);
      }
 };
+
+/*  Max Dot Product of Two Subsequences */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+     int n, m;
+     vector<int> ans1, ans2;
+     vector<vector<int>> dp;
+
+     int solve(int i, int j)
+     {
+          if (i == n || j == m)
+               return INT_MIN;
+
+          if (dp[i][j] != INT_MIN)
+               return dp[i][j];
+
+          int take = ans1[i] * ans2[j];
+          int next = solve(i + 1, j + 1);
+          if (next > 0)
+               take += next;
+
+          int skip1 = solve(i + 1, j);
+          int skip2 = solve(i, j + 1);
+
+          return dp[i][j] = max({take, skip1, skip2});
+     }
+
+     int maxDotProduct(vector<int> &nums1, vector<int> &nums2)
+     {
+          ans1 = nums1;
+          ans2 = nums2;
+          n = ans1.size();
+          m = ans2.size();
+
+          dp.assign(n, vector<int>(m, INT_MIN));
+          return solve(0, 0);
+     }
+};
