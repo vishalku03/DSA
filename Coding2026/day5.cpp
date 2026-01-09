7bc27197a0fc5d75ccd69784dc3cb61b76ebb515
vishaku03 <vishalk.shrivastav1@gmail.com>
2026-01-09 17:09:13 +0530

    leetcode

--- Coding2026/day5.cpp
+++ Coding2026/day5.cpp
@@ -34,3 +34,44 @@ public:
           return ans;
      }
 };
+
+/*Smallest Subtree with all the Deepest Nodes*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+  struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+ 
+class Solution
+{
+public:
+     int maxDepth = 0;
+     TreeNode *lca = NULL;
+
+     int dfs(TreeNode *root, int depth)
+     {
+          maxDepth = max(maxDepth, depth);
+          if (!root)
+               return depth;
+
+          int left = dfs(root->left, depth + 1);
+          int right = dfs(root->right, depth + 1);
+
+          if (left == maxDepth && right == maxDepth)
+               lca = root;
+          return max(left, right);
+     }
+
+     TreeNode *subtreeWithAllDeepest(TreeNode *root)
+     {
+          dfs(root, 0);
+          return lca;
+     }
+};
