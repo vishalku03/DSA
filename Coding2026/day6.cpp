11cea20ca9be48ea6ee0c755be0e652b0c196093
vishaku03 <vishalk.shrivastav1@gmail.com>
2026-01-10 18:27:14 +0530

    LC

--- Coding2026/day6.cpp
+++ Coding2026/day6.cpp
@@ -40,3 +40,39 @@ public:
           return AtMostK(s, k) - AtMostK(s, k - 1);
      }
 };
+
+/*  Minimum ASCII Delete Sum for Two Strings  */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+     int minimumDeleteSum(string s1, string s2)
+     {
+          int n = s1.size();
+          int m = s2.size();
+
+          vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+
+          for (int i = 1; i <= n; i++)
+          {
+               for (int j = 1; j <= m; j++)
+               {
+                    if (s1[i - 1] == s2[j - 1])
+                         dp[i][j] = dp[i - 1][j - 1] + s1[i - 1];
+                    else
+                         dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+               }
+          }
+
+          int sum = 0;
+          for (char c : s1)
+               sum += c;
+          for (char c : s2)
+               sum += c;
+
+          return sum - 2 * dp[n][m];
+     }
+};
