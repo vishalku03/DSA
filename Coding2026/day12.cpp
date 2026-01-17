178dfc0b09c9c4851b8ecd4ca7157f8bc86ff51e
vishaku03 <vishalk.shrivastav1@gmail.com>
2026-01-17 06:00:54 +0530

    gfg

---
+++ Coding2026/day12.cpp
@@ -0,0 +1,43 @@
+/*Expression contains redundant bracket or not */
+
+#include<bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+     bool isOperator(char c)
+     {
+          return (c == '+' || c == '-' || c == '*' || c == '/');
+     }
+
+     bool checkRedundancy(string &s)
+     {
+          stack<char> st;
+
+          for (char c : s)
+          {
+
+               if (c == '(' || isOperator(c))
+               {
+                    st.push(c);
+               }
+               else if (c == ')')
+               {
+                    if (st.top() == '(')
+                    {
+                         return true;
+                    }
+                    else
+                    {
+                         while (!st.empty() && st.top() != '(')
+                         {
+                              st.pop();
+                         }
+                         st.pop();
+                    }
+               }
+          }
+
+          return false;
+     }
+};
