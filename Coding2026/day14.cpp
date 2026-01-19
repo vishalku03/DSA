0209d58332ea843bd1e74f2ea1c0e93e7ed78199
vishaku03 <vishalk.shrivastav1@gmail.com>
2026-01-19 13:35:05 +0530

    gfg

---
+++ Coding2026/day14.cpp
@@ -0,0 +1,41 @@
+
+/* Remove K Digits */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+     string removeKdig(string &s, int k)
+     {
+          stack<char> st;
+          string ans = "";
+          int count = 0;
+          for (auto &i : s)
+          {
+               while (!st.empty() && st.top() > i && count < k)
+               {
+                    st.pop();
+                    count++;
+               }
+               if (st.empty() && i == '0')
+                    continue;
+               st.push(i);
+          }
+          while (!st.empty() && count < k)
+          {
+               st.pop();
+               count++;
+          }
+          while (!st.empty())
+          {
+               ans += st.top();
+               st.pop();
+          }
+          reverse(ans.begin(), ans.end());
+          if (ans.size() == 0)
+               return "0";
+          return ans;
+     }
+};
