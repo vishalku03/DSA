688b863bd2e6a6721f1faaee5a9bb5cfb1239b81
vishaku03 <vishalk.shrivastav1@gmail.com>
2026-02-05 19:04:18 +0530

    GFG

---
+++ Coding2026/day25.cpp
@@ -0,0 +1,39 @@
+/*Maximize Number of 1's*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+     int maxOnes(vector<int> &arr, int k)
+     {
+          int n = arr.size();
+          int ans = 0;
+          int i = 0;
+          int j = 0;
+          while (j < n)
+          {
+               if (arr[j++] == 0)
+                    k--;
+
+               while (k < 0)
+               {
+                    if (arr[i++] == 0)
+                         k++;
+               }
+
+               ans = max(ans, j - i);
+          }
+
+          return ans;
+     }
+};
+int main()
+{
+     Solution sol;
+     vector<int> arr = {1, 0, 0, 1, 1, 0, 1};
+     int k = 2;
+     cout << sol.maxOnes(arr, k) << endl; // Output: 6
+     return 0;
+}
