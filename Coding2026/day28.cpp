5f621daf21c66791c13568b0b59994de2031f804
vishaku03 <vishalk.shrivastav1@gmail.com>
2026-02-09 03:36:37 +0530

    Leetcode

--- Coding2026/day28.cpp
+++ Coding2026/day28.cpp
@@ -31,3 +31,44 @@ public:
           return ans;
      }
 };
+
+/*110. Balanced Binary Tree*/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+     bool isBalanced(TreeNode *root)
+     {
+
+          bool result = true;
+
+          solve(root, result);
+          return result;
+     }
+
+     int solve(TreeNode *root, bool &balanced)
+     {
+          if (root == NULL || balanced == false)
+               return 0;
+
+          int leftSide = solve(root->left, balanced);
+          int rightSide = solve(root->right, balanced);
+
+          if (abs(leftSide - rightSide) > 1)
+          {
+               balanced = false;
+          }
+
+          return max(leftSide, rightSide) + 1;
+     }
+};
