21a1d96637244790e7890bb779d0bc303d0d129e
vishaku03 <vishalk.shrivastav1@gmail.com>
2026-02-21 22:59:10 +0530

    GFG

---
+++ Coding2026/day38.cpp
@@ -0,0 +1,18 @@
+/*Find H-Index*/
+
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+  public:
+    int hIndex(vector<int>& citations) {
+         int n = citations.size();
+        sort(citations.begin(), citations.end());
+        int ans=0;
+        
+         for(int i=0; i<n; i++){
+            ans = max(ans, min(citations[i], n-i));
+        }
+    
+        return ans;
+    }
+};
