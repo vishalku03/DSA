4e644afeb87cda3997e2ea72c99a611840ea2741
vishaku03 <vishalk.shrivastav1@gmail.com>
2026-02-25 19:11:55 +0530

    GFG

---
+++ Coding2026/day41.cpp
@@ -0,0 +1,23 @@
+
+/*Longest Subarray with Majority Greater than K*/
+#include<bits/stdc++.h>
+using  namespace std;
+
+class Solution {
+  public:
+    int longestSubarray(vector<int> &arr, int k) {
+        int n = arr.size(), ans = 0, ct = 0;
+        unordered_map<int, int> mp;
+        
+        for(int i=0; i<n; i++) {
+            if(arr[i]>k) ct++;
+            else ct--;
+            
+            if(ct > 0) ans = max(ans, i+1);
+            else if(mp.find(ct-1)!=mp.end()) ans = max(ans, i-mp[ct-1]);
+            
+            if(mp.find(ct)==mp.end()) mp[ct]=i;
+        }
+        return ans;
+    }
+};
